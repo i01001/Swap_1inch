891839f95a4e6d58e01572e1819d9360ee279bb8
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-10 14:55:42 +0400

    June 10, 2022 2:55 PM

--- 1inch_swap.js
+++ 1inch_swap.js
@@ -25,11 +25,11 @@ async function quotes() {
 async function swappingFTMtoDAI(){
   try {
     const swap_FTMtoDAI = await axios.get('https://api.1inch.io/v4.0/250/swap?fromTokenAddress=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE&toTokenAddress=0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E&amount=100000000000000000&fromAddress=0xaF87B6479f9CA8D3BAE56deAd220bcE44a709549&slippage=0.1&disableEstimate=true');
-    console.log(swap_FTMtoDAI);
-    if(swappingFTMtoDAI.data){
+    // console.log(swap_FTMtoDAI);
+    if(swap_FTMtoDAI.data){
       swap_data = swap_FTMtoDAI.data;
       swap_data.tx.gas = 1000000;
-      console.log(swap_data.tx);
+      console.log("1", swap_data.tx);
     }
   }catch (swapFTMtoDAIerror){
     console.log("Error swapping FTM to DAI", swapFTMtoDAIerror);
