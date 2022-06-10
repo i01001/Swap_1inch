9b1df9af4aa61a8b3397adf76fae9f596bc79a3a
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-10 16:29:48 +0400

    June 10, 2022 4:29 PM

--- 1inch_swap.js
+++ 1inch_swap.js
@@ -90,7 +90,7 @@ async function main() {
     fromTokenAddress='0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E'
     toAddressAddress='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
     tokenAmount = 100000000000000000; 
-    let contract = await web3.eth.contract(minABI).at(fromTokenAddress);
+    let contract = new web3.eth.Contract(minABI).at(fromTokenAddress);
 
     contract.balanceOf(walletAddress, (error, balance) => {
       // Get decimals
