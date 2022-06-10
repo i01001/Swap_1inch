975aa638c288f228605ccb8fc94a1d85194065e7
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-10 16:45:06 +0400

    June 10, 2022 4:45 PM

--- 1inch_swap.js
+++ 1inch_swap.js
@@ -1,6 +1,6 @@
-const Web3 = require('web3');
-const axios = require('axios');
-require('dotenv').config();
+const Web3 = require("web3");
+const axios = require("axios");
+require("dotenv").config();
 
 const RPC_URL_FANTOM_MAINNET = process.env.RPC_URL_FANTOM_MAINNET;
 const PRIVATE_KEY_FTM = process.env.PRIVATE_KEY_FTM;
@@ -8,104 +8,428 @@ const PRIVATE_KEY_FTM = process.env.PRIVATE_KEY_FTM;
 const web3 = new Web3(RPC_URL_FANTOM_MAINNET);
 const wallet = web3.eth.accounts.wallet.add(PRIVATE_KEY_FTM);
 
-let minABI = [
-  // balanceOf
+// let minABI = [
+//   // balanceOf
+//   {
+//     "constant":true,
+//     "inputs":[{"name":"_owner","type":"address"}],
+//     "name":"balanceOf",
+//     "outputs":[{"name":"balance","type":"uint256"}],
+//     "type":"function"
+//   },
+//   // decimals
+//   {
+//     "constant":true,
+//     "inputs":[],
+//     "name":"decimals",
+//     "outputs":[{"name":"","type":"uint8"}],
+//     "type":"function"
+//   }
+// ];
+
+let ABI_DAI = [
   {
-    "constant":true,
-    "inputs":[{"name":"_owner","type":"address"}],
-    "name":"balanceOf",
-    "outputs":[{"name":"balance","type":"uint256"}],
-    "type":"function"
+    inputs: [
+      { internalType: "string", name: "_name", type: "string" },
+      { internalType: "string", name: "_symbol", type: "string" },
+      { internalType: "uint8", name: "_decimals", type: "uint8" },
+      { internalType: "address", name: "_owner", type: "address" },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
   },
-  // decimals
   {
-    "constant":true,
-    "inputs":[],
-    "name":"decimals",
-    "outputs":[{"name":"","type":"uint8"}],
-    "type":"function"
-  }
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "oldOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "effectiveTime",
+        type: "uint256",
+      },
+    ],
+    name: "LogChangeDCRMOwner",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "bytes32",
+        name: "txhash",
+        type: "bytes32",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "LogSwapin",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "bindaddr",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "LogSwapout",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "from", type: "address" },
+      { indexed: true, internalType: "address", name: "to", type: "address" },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "DOMAIN_SEPARATOR",
+    outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "PERMIT_TYPEHASH",
+    outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "bytes32", name: "txhash", type: "bytes32" },
+      { internalType: "address", name: "account", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
+    ],
+    name: "Swapin",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amount", type: "uint256" },
+      { internalType: "address", name: "bindaddr", type: "address" },
+    ],
+    name: "Swapout",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "TRANSFER_TYPEHASH",
+    outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "address", name: "", type: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "value", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "value", type: "uint256" },
+      { internalType: "bytes", name: "data", type: "bytes" },
+    ],
+    name: "approveAndCall",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+    name: "changeDCRMOwner",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "nonces",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "target", type: "address" },
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "value", type: "uint256" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+      { internalType: "uint8", name: "v", type: "uint8" },
+      { internalType: "bytes32", name: "r", type: "bytes32" },
+      { internalType: "bytes32", name: "s", type: "bytes32" },
+    ],
+    name: "permit",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "value", type: "uint256" },
+    ],
+    name: "transfer",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "value", type: "uint256" },
+      { internalType: "bytes", name: "data", type: "bytes" },
+    ],
+    name: "transferAndCall",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "from", type: "address" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "value", type: "uint256" },
+    ],
+    name: "transferFrom",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "target", type: "address" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "value", type: "uint256" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+      { internalType: "uint8", name: "v", type: "uint8" },
+      { internalType: "bytes32", name: "r", type: "bytes32" },
+      { internalType: "bytes32", name: "s", type: "bytes32" },
+    ],
+    name: "transferWithPermit",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ];
 
 async function quotes() {
   try {
-      const quote = await axios.get('https://api.1inch.io/v4.0/250/quote?fromTokenAddress=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE&toTokenAddress=0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E&amount=100000000000000000');
-      console.log(quote);
-      if (quote){
-        name1 = await quote.data.estimatedGas;
-        console.log(await name1);
-      }
-    } catch (error) {
+    const quote = await axios.get(
+      "https://api.1inch.io/v4.0/250/quote?fromTokenAddress=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE&toTokenAddress=0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E&amount=100000000000000000"
+    );
+    console.log(quote);
+    if (quote) {
+      name1 = await quote.data.estimatedGas;
+      console.log(await name1);
+    }
+  } catch (error) {
     console.log("Quote execution error", error);
   }
 }
 
-
-async function approval(_tokenAddress, _tokenAmount){
-  try{
-    const approve = await axios.get('https://api.1inch.io/v4.0/250/approve/transaction?tokenAddress=0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E&amount=100000000000000000');
+async function approval(_tokenAddress, _tokenAmount) {
+  try {
+    const approve = await axios.get(
+      "https://api.1inch.io/v4.0/250/approve/transaction?tokenAddress=0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E&amount=100000000000000000"
+    );
     console.log(approve);
-    if(approve.data){
+    if (approve.data) {
       approve_data = approve.data;
       approve_data.gas = 1000000;
       approve_data.from = wallet.address;
       transaction = await web3.eth.sendTransaction(approve_data);
-      if(transaction.status){
+      if (transaction.status) {
         console.log("approval for DAI successful", _tokenAmount);
-      }
-      else{
+      } else {
         console.log("Approval Transaction unsuccessful", _tokenAmount);
       }
     }
-  }catch(error_approval){
+  } catch (error_approval) {
     console.log("Error approval", _tokenAmount);
   }
-  }
+}
 
-async function swappingFTMtoDAI(_fromTokenAddress, _toTokenAddress, _tokenAmount){
-
-  if(_fromTokenAddress != '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'){
+async function swappingFTMtoDAI(
+  _fromTokenAddress,
+  _toTokenAddress,
+  _tokenAmount
+) {
+  if (_fromTokenAddress != "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE") {
     await approval(_toTokenAddress, _tokenAmount);
   }
   try {
-    const swap_FTMtoDAI = await axios.get(`https://api.1inch.io/v4.0/250/swap?fromTokenAddress=${_fromTokenAddress}&toTokenAddress=${_toTokenAddress}&amount=${_tokenAmount}&fromAddress=${wallet.address}&slippage=0.1&disableEstimate=true`);
+    const swap_FTMtoDAI = await axios.get(
+      `https://api.1inch.io/v4.0/250/swap?fromTokenAddress=${_fromTokenAddress}&toTokenAddress=${_toTokenAddress}&amount=${_tokenAmount}&fromAddress=${wallet.address}&slippage=0.1&disableEstimate=true`
+    );
     // console.log(swap_FTMtoDAI);
-    if(swap_FTMtoDAI.data){
+    if (swap_FTMtoDAI.data) {
       swap_data = swap_FTMtoDAI.data;
       swap_data.tx.gas = 1000000;
       // console.log("1", swap_data.tx);
       transaction = await web3.eth.sendTransaction(swap_data.tx);
-      if(transaction.status){
+      if (transaction.status) {
         console.log("Swap FTM to DAI successful");
       }
     }
-  }catch (swapFTMtoDAIerror){
+  } catch (swapFTMtoDAIerror) {
     console.log("Error swapping FTM to DAI", swapFTMtoDAIerror);
   }
 }
 
-
 async function main() {
-    // await quotes();
-    fromTokenAddress='0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E'
-    toAddressAddress='0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
-    tokenAmount = 100000000000000000; 
-    // let contract = new web3.eth.Contract(minABI).at(fromTokenAddress);
+  // await quotes();
+  fromTokenAddress = "0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E";
+  toAddressAddress = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+  tokenAmount = 100000000000000000;
+  // let contract = new web3.eth.Contract(minABI).at(fromTokenAddress);
 
-    // contract.balanceOf(walletAddress, (error, balance) => {
-    //   // Get decimals
-    //   contract.decimals((error, decimals) => {
-    //     // calculate a balance
-    //     balance = balance.div(10**decimals);
-    //     console.log(balance.toString());
-    //   });
-    // });
+  // contract.balanceOf(walletAddress, (error, balance) => {
+  //   // Get decimals
+  //   contract.decimals((error, decimals) => {
+  //     // calculate a balance
+  //     balance = balance.div(10**decimals);
+  //     console.log(balance.toString());
+  //   });
+  // });
 
-    let balance = await fromTokenAddress.balanceOf(wallet.address);
-    console.log(balance);
+  let balance = await fromTokenAddress.balanceOf(wallet.address);
+  console.log(balance);
 
-    // await swappingFTMtoDAI(fromTokenAddress, toAddressAddress, tokenAmount);
-
+  // await swappingFTMtoDAI(fromTokenAddress, toAddressAddress, tokenAmount);
 }
 
 main();
