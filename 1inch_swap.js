37966f52c685f87bceb9cc201f5822780b5cb1fd
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-10 16:56:52 +0400

    June 10, 2022 4:56 PM

--- 1inch_swap.js
+++ 1inch_swap.js
@@ -433,7 +433,7 @@ async function main() {
   //   });
   // });
 
-  let balance = await contract(fromTokenAddress).methods.balanceOf(wallet.address);
+  let balance = await (fromTokenAddress).methods.balanceOf(wallet.address);
   console.log(balance);
 
   // await swappingFTMtoDAI(fromTokenAddress, toAddressAddress, tokenAmount);
